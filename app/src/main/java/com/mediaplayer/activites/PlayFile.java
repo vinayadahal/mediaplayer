33590aa7f2524035f40bff3b4bb4415edd2d6ddf
Vinaya Dahal <vinayadahal@gmail.com>
2016-08-18 13:32:43 +0545

    progress timer working. code cleanup needed

--- app/src/main/java/com/mediaplayer/activites/PlayFile.java
+++ app/src/main/java/com/mediaplayer/activites/PlayFile.java
@@ -3,6 +3,7 @@ package com.mediaplayer.activites;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
@@ -45,8 +46,6 @@ public class PlayFile extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN); //hides notification bar
         setContentView(R.layout.activity_play_file);
         Bundle bundle = getIntent().getExtras();
-
-
         if (bundle != null) {
             filePath = bundle.getString("fileName");
             videoFileName = bundle.getString("videoFileName");
@@ -54,8 +53,7 @@ public class PlayFile extends AppCompatActivity {
             allFileList = bundle.getStringArrayList("allFileList");
         }
         playFile(filePath, videoFileName);
-
-        for (int i = 0; i < allVideoPath.size(); i++) {
+        for (int i = 0; i < allVideoPath.size(); i++) { // foe
             allFilesPath.add(allVideoPath.get(i) + allFileList.get(i));
         }
         playBtn = (ImageButton) findViewById(R.id.play_btn);
@@ -68,7 +66,6 @@ public class PlayFile extends AppCompatActivity {
         pauseBtn.setVisibility(View.VISIBLE);
         originalBtn.setVisibility(View.VISIBLE);
         fullscreenBtn.setVisibility(View.GONE);
-        new PlayerSupport().playerScreenTouch((RelativeLayout) findViewById(R.id.play_file_relative_layout), title_control, notification_txt);
     }
 
     public void playFile(String filename, String videoFileName) {
@@ -85,6 +82,8 @@ public class PlayFile extends AppCompatActivity {
                 duration = videoView.getDuration();
                 mediaPlayer = mp; // used to pause video
                 progressBarUpdate();
+//                timeUpdate();
+//                new PlayerSupport().timeUpdate(duration, (TextView) findViewById(R.id.current_time), videoView, title_control);
             }
         });
         videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
@@ -96,12 +95,42 @@ public class PlayFile extends AppCompatActivity {
     }
 
     public void progressBarUpdate() {
-        new PlayerSupport().updateProgressBar((ProgressBar) findViewById(R.id.vid_progressbar), videoView, duration);
+        new PlayerSupport().playerScreenTouch((RelativeLayout) findViewById(R.id.play_file_relative_layout),
+                title_control, notification_txt, (ProgressBar) findViewById(R.id.vid_progressbar), videoView,
+                duration, (TextView) findViewById(R.id.current_time));
         int minutes = (int) (duration / 1000) / 60;
         int seconds = (int) (duration / 1000) % 60;
         TextView totalTime = (TextView) findViewById(R.id.total_time);
         totalTime.setText(minutes + ":" + seconds);
     }
+
+//    public void timeUpdate() {
+//        final TextView currentTimeTxt = (TextView) findViewById(R.id.current_time);
+//        final Handler handler = new Handler();
+//        Runnable runnable = new Runnable() {
+//            public void run() {
+//                System.out.println("inside thread");
+//                long current = 0;
+//                int sleepingFor = 0;
+//                while (current != duration) {
+//                    current = videoView.getCurrentPosition();
+//                    if (sleepingFor == 6 || current == duration || title_control.getVisibility() != View.VISIBLE) {
+//                        break;
+//                    }
+//                    final int minutes = (int) (current / 1000) / 60;
+//                    final int seconds = (int) (current / 1000) % 60;
+//                    System.out.println("TIME::: " + minutes + ":" + seconds);
+//                    handler.post(new Runnable() {
+//                        public void run() {
+//                            currentTimeTxt.setText(minutes + ":" + seconds);
+//                        }
+//                    });
+//                    System.out.println("Sleeping for ---------> " + sleepingFor);
+//                }
+//            }
+//        };
+//        new Thread(runnable).start();
+//    }
 
     public void pauseVideo(View view) {
         playBtn.setVisibility(View.VISIBLE);
@@ -129,8 +158,6 @@ public class PlayFile extends AppCompatActivity {
     }
 
     public void previous(View view) {
-        System.out.println("current filePath:::: " + filePath);
-        System.out.println("INDEX of:::::::::::::::::::: " + allFilesPath.indexOf(filePath));
         int nextFile = allFilesPath.indexOf(filePath) - 1; //current file - 1
         if (nextFile < 0) {
             nextFile = allFileList.size() - 1; // setting final value as next file so that player support  backward loop
@@ -141,8 +168,6 @@ public class PlayFile extends AppCompatActivity {
 
 
     public void next(View view) {
-        System.out.println("current filePath:::: " + filePath);
-        System.out.println("INDEX of:::::::::::::::::::: " + allFilesPath.indexOf(filePath));
         int nextFile = allFilesPath.indexOf(filePath) + 1; //current file + 1
         if (nextFile > (allFilesPath.size() - 1)) {
             nextFile = 0; // setting 0 as next file so that player support forward loop
--- app/src/main/java/com/mediaplayer/services/PlayerSupport.java
+++ app/src/main/java/com/mediaplayer/services/PlayerSupport.java
@@ -1,7 +1,7 @@
 package com.mediaplayer.services;
 
 
-import android.content.Context;
+import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
@@ -14,12 +14,16 @@ import com.mediaplayer.components.Effects;
 
 public class PlayerSupport {
 
-    public void playerScreenTouch(RelativeLayout relativeLayout, final RelativeLayout title_control, final TextView notification_txt) {
-        relativeLayout.setOnTouchListener(new View.OnTouchListener() {
+    public void playerScreenTouch(RelativeLayout play_file_rl, final RelativeLayout title_control,
+                                  final TextView notification_txt, final ProgressBar progressBar,
+                                  final VideoView videoView, final long duration, final TextView currentTimeTxt) {
+        play_file_rl.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
+                        updateProgressBar(progressBar, videoView, duration, title_control);
+                        timeUpdate(duration, currentTimeTxt, videoView, title_control);
                         title_control.setVisibility(View.VISIBLE);
                         break;
                     case MotionEvent.ACTION_UP:
@@ -38,31 +42,104 @@ public class PlayerSupport {
         });
     }
 
-    public void updateProgressBar(final ProgressBar progressBar, final VideoView videoView, final long duration) {
+    public void updateProgressBar(final ProgressBar progressBar, final VideoView videoView, final long duration, final RelativeLayout title_control) {
         Thread progressUpdate = new Thread() {
             public void run() {
                 progressBar.setProgress(0);
                 progressBar.setMax(100);
-                int i = 0;
+                int sleepingFor = 0;
                 while (progressBar.getProgress() <= 100) {
                     long current = videoView.getCurrentPosition();
                     int currentTimeInSec = (int) (current * 100 / duration);
                     progressBar.setProgress(currentTimeInSec);
-                    if (current == 0) {
-                        if (i >= 15000) {
-                            i = 0;
-                            break;
-                        }
-                    }
-                    if (progressBar.getProgress() >= 100) {
+                    if (current == 0 || sleepingFor == 6 || progressBar.getProgress() >= 100) {
                         break;
                     }
-
+                    if (title_control.getVisibility() == View.VISIBLE) {
+                        if (this.getState() == State.TIMED_WAITING) {
+                            notify();
+                        }
+                    } else {
+                        try {
+                            sleep(1000);
+                            sleepingFor++;
+                        } catch (InterruptedException ex) {
+                            System.out.println("InterruptedException ::: " + ex);
+                        }
+                    }
                 }
             }
         };
         progressUpdate.start();
     }
+
+
+    public void timeUpdate(final long duration, final TextView currentTimeTxt, final VideoView videoView, final RelativeLayout title_control) {
+        final Handler handler = new Handler();
+        Thread th = new Thread() {
+            @Override
+            public void run() {
+                System.out.println("inside thread");
+                long current = 0;
+                int sleepingFor = 0;
+                while (current != duration) {
+                    current = videoView.getCurrentPosition();
+                    if (sleepingFor == 6 || current == duration) {
+                        break;
+                    }
+                    if (title_control.getVisibility() == View.VISIBLE) {
+                        if (this.getState() == State.TIMED_WAITING) {
+                            notify();
+                        }
+                    } else {
+                        try {
+                            sleep(1000);
+                            sleepingFor++;
+                        } catch (InterruptedException ex) {
+                            System.out.println("InterruptedException ::: " + ex);
+                        }
+                    }
+                    final int minutes = (int) (current / 1000) / 60;
+                    final int seconds = (int) (current / 1000) % 60;
+                    System.out.println("TIME::: " + minutes + ":" + seconds);
+                    handler.post(new Runnable() {
+                        public void run() {
+                            currentTimeTxt.setText(minutes + ":" + seconds);
+                        }
+                    });
+                    System.out.println("Sleeping for ---------> " + sleepingFor);
+                }
+            }
+        };
+        th.start();
+
+
+//        final Handler handler = new Handler();
+//        Runnable runnable = new Runnable() {
+//            public void run() {
+//                System.out.println("inside thread");
+//                long current = 0;
+//                int sleepingFor = 0;
+//                while (current != duration) {
+//                    current = videoView.getCurrentPosition();
+//                    if (sleepingFor == 6 || current == duration) {
+//                        break;
+//                    }
+//                    final int minutes = (int) (current / 1000) / 60;
+//                    final int seconds = (int) (current / 1000) % 60;
+//                    System.out.println("TIME::: " + minutes + ":" + seconds);
+//                    handler.post(new Runnable() {
+//                        public void run() {
+//                            currentTimeTxt.setText(minutes + ":" + seconds);
+//                        }
+//                    });
+//                    System.out.println("Sleeping for ---------> " + sleepingFor++);
+//                }
+//            }
+//        };
+//        new Thread(runnable).start();
+    }
+
 
     public void setOriginalSize(RelativeLayout.LayoutParams layoutParams, VideoView vv, ImageButton fullBtn, ImageButton oriBtn, TextView txtView) {
         layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, 0);
